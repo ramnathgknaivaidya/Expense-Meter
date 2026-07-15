5d3c1546f06a293dbeece64e3f1782d21545c6db
ramnathgknaivaidya <ramnathgk.naivaidya@gmail.com>
2026-07-15 14:32:47 +0530

    Add files via upload

---
+++ backend/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const { bcryptRounds } = require('../config/environment');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: [true, 'Name is required'], trim: true },
+  email: { type: String, required: [true, 'Email is required'], unique: true, lowercase: true, trim: true },
+  password: { type: String, required: [true, 'Password is required'], minlength: 6 },
+  currency: { type: String, default: 'INR', enum: ['INR', 'USD', 'EUR', 'GBP'] },
+  profileImage: { type: String, default: '' },
+}, { timestamps: true });
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, bcryptRounds);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+userSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
+module.exports = mongoose.model('User', userSchema);
